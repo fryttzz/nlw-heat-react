54500d0e93850f61cb577c64c45b22aa9a3419b2
Matheus Santos <matheusps0012@gmail.com>
2021-10-21 18:37:32 -0300

    Adding - Api access

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,6 +1,24 @@
 import styles from "./styles.module.scss";
 import logoImg from "../../assets/logo.svg";
+import { api } from "../../services/api";
+import { useEffect, useState } from "react";
+type Message = {
+  id: string;
+  text: string;
+  user: {
+    name: string;
+    avatar_url: string;
+  };
+};
+
 export function MessageList() {
+  const [messages, setMessages] = useState<Message[]>([]);
+
+  useEffect(() => {
+    api.get<Message[]>("/messages/last3").then((response) => {
+      setMessages(response.data);
+    });
+  }, []);
   return (
     <div className={styles.messageListWrapper}>
       <img src={logoImg} alt="DoWhile 2021" />
